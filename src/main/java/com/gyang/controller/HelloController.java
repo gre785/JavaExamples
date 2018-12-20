4cfb559b69c6eb6d7be48fdf2ca6c0bc675d4c74
Guang Yang <gyang@parasoft.com>
2018-12-20 14:12:52 -0800

    init maven project

---
+++ src/main/java/com/gyang/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.gyang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController
+{
+    @RequestMapping("/boot/hello")
+    public @ResponseBody String getBody()
+    {
+        return "Hello Spring Boot!";
+    }
+}
---
+++ src/main/java/com/gyang/exmaples/lambda/Operator.java
@@ -0,0 +1,6 @@
+package com.gyang.exmaples.lambda;
+
+public interface Operator
+{
+    public void perform(String message);
+}
