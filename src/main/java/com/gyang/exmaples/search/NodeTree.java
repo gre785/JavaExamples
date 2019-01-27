5fdb64fdfb61d0f5ea37d3e11e536f63b3a8fa38
gyang <gre785@gmail.com>
2019-01-27 10:37:09 -0800

    adding sorts

---
+++ src/main/java/com/gyang/exmaples/search/NodeTree.java
@@ -0,0 +1,13 @@
+
+package com.gyang.exmaples.search;
+
+public class NodeTree
+{
+    public int value;
+    public NodeTree left, right;
+
+    public NodeTree(int value)
+    {
+        this.value = value;
+    }
+}
