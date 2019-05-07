e0a363d9687a5ddfe905634778178b390ab1f202
Guang Yang <gyang@parasoft.com>
2019-05-07 08:51:05 -0700

    read all words in a file and get descriptions from dictionary

--- src/main/java/com/gyang/exmaples/util/FileUtil.java
+++ src/main/java/com/gyang/exmaples/util/FileUtil.java
@@ -1,32 +1,48 @@
+
 package com.gyang.exmaples.util;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileUtil
 {
     public static int counter = 0;
+
     public FileUtil()
     {
         // util class, don't instantiate it.
     }
 
-    public static void readFile(String path)
+    public static List<String> readFile(String path)
     {
         File file = new File(path);
 
+        List<String> result = new ArrayList<>();
         BufferedReader br = null;
         try {
             br = new BufferedReader(new FileReader(file));
             String st;
             while ((st = br.readLine()) != null) {
-                counter ++;
-                System.out.println(st);
+                counter++;
+                if (st.length() < 2) {
+                    continue;
+                }
+                for (String string : st.split(",")) {
+                    result.add(string.replaceAll("\\s", ""));
+                }
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return result;
     }
 
     private static class Person
