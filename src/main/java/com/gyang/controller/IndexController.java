61e9c2d75786ee41670d8857b4aa3f0b2646c872
Guang Yang <gyang@parasoft.com>
2019-06-07 15:36:22 -0700

    crawler + hibernate

--- src/main/java/com/gyang/controller/IndexController.java
+++ src/main/java/com/gyang/controller/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ThymeleafIndexController
+public class IndexController
 {
     @RequestMapping("/boot/index")
     public String index(Model model)
