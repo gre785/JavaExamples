0c2f97c95f1f448d6d45b13a92b47f034f57d8b2
gre986 <gre9986@gmail.com>
2019-11-23 12:52:55 -0800

    adding empty lines

--- src/main/java/com/gyang/Example.java
+++ src/main/java/com/gyang/Example.java
@@ -6,6 +6,9 @@ public final class Example
     public final void setValue(boolean newValue)
     {
         StaticExample.setValue(newValue);
+        
+        
+        
 //    	Boolean.getBoolean("name");
     }
 }
