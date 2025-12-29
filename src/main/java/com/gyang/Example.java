5bf2b6dbcdb22a83ae0ac816a3742a99215e8413
Yang, Guang <gre785@example.com>
2025-12-28 22:31:22 -0500

    ccz - 2014

--- src/main/java/com/gyang/Example.java
+++ src/main/java/com/gyang/Example.java
@@ -1,11 +1,23 @@
 
 package com.gyang;
 
+import java.io.ObjectInputFilter.Config;
+
 public final class Example {
-	public final void setValue(boolean newValue) {
+	public final boolean setValue(boolean newValue) {
 		StaticExample.setValue(newValue);
 
 		System.setProperty("name", "zijun");
 		System.setProperty("gender", "female");
+		
+		boolean x = newValue;	
+		if ( x == true) {
+		    return true;
+		}
+		else {
+		    return false;
+		}
+		        
+	
 	}
 }
